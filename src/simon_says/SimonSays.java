1c66802b5cbc7dcfd1e3ddf39d485bc507d6a06e
ianbrowne10 <league@wts20.attlocal.net>
2019-04-09 18:27:09 -0700

    NastySurprise, SimonSays (IP) :sunglasses:

--- src/simon_says/SimonSays.java
+++ src/simon_says/SimonSays.java
@@ -30,17 +30,20 @@ public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
-
+JFrame frame = new JFrame();
 	public void run() {
 		// 2. Add the four images that match keyboard keys like this:
 		// images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
-
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_UP), "down.jpg");
+		images.put(new Integer(KeyEvent.VK_UP), "left.jpg");
+		images.put(new Integer(KeyEvent.VK_UP), "right.jpg");
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching
 		// key when
 		// 'Simon says' otherwise press a different key"
-
+JOptionPane.showMessageDialog(null, "Press the matching key on the keyboard when simon says. Otherwise, if simon doesn't say, press a different key.");
 		// 4. Call the showImage method to show an image
-
+showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -76,19 +79,19 @@ public class SimonSays extends KeyAdapter {
 		// 5. Initialize your frame to a new JFrame()
 
 		// 6. Set the frame to visible
-
+frame.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		// frame.add(getNextRandomImage());
+		 frame.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
-
+frame.setTitle("Simon Says");
 		// 9. Pack the frame
-
+frame.pack();
 		// 10. Set the defaultCloseOperation of your from to
 		// JFrame.EXIT_ON_CLOSE
-
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+frame.addKeyListener(this);
 		// 12. Create a new instance of Random
 
 		// 13. Use the Random and the speak method to either say
